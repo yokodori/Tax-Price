d471fa325909887da62c6d2bd29b9359b1b48af3
yokodori <yokodori4@gmail.com>
2025-07-25 10:11:54 +0900

    feat: 税込と税額を分けて格納・表示する仕組みを追加

--- taxrate.php
+++ taxrate.php
@@ -1,9 +1,13 @@
 <?php
-function taxrate_10(int $origin_price): int {
-    return round($origin_price * 1.1);
+function taxrate_10(int $origin_price): array {
+    return [
+        'tax_price' => round($origin_price * 0.1),
+        'include_tax' => round($origin_price * 1.1),
+    ];
 }
 
 $origin_price = 500;
-$include_tax = taxrate_10($origin_price);
-echo ("私は{$include_tax}円を支払います。");
+$tax_price = taxrate_10($origin_price)['tax_price'];
+$include_tax = taxrate_10($origin_price)['include_tax'];
+echo ("私は{$include_tax}円を支払います。そのうち税額は{$tax_price}です。");
 ?>
